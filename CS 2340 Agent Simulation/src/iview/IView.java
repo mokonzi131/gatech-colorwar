351b90df7306613178c792a5d5ccb3c54d7df9fc
Michael Landes <mokonzi131@gmail.com>
2014-03-31 19:46:41 -0400

    added javadoc descriptions to the view interface

--- CS 2340 Agent Simulation/src/iview/IView.java
+++ CS 2340 Agent Simulation/src/iview/IView.java
@@ -3,19 +3,25 @@ package iview;
 import java.awt.image.BufferedImage;
 
 public interface IView {
-	// takes an image from the environment describing the full game
-	//  and displays it (using swing)
+	/**
+	 * Displays an environment image to the screen (using swing)
+	 * @param image describing the full game environment
+	 */
 	public void renderEnvironment(BufferedImage image);
 	
-	// takes an image from the agent describing their immediate
-	//  surroundings and displays it (using swing)
+	/**
+	 * Displays and agent image to the screen (using swing)
+	 * @param image describing the immediate environment of an agent
+	 */
 	public void renderAgent(BufferedImage image);
 	
-	// polls the input mechanism (keyboard for now) and returns an integer
-	//  representing a cardinal direction where:
-	// 0 = LEFT
-	// 1 = UP
-	// 2 = RIGHT
-	// 3 = DOWN
+	/**
+	 * Polls the input device (keyboard for now) for a direction
+	 * @return integer representing a cardinal direction where
+	 *  0 = LEFT,
+	 *  1 = UP,
+	 *  2 = RIGHT,
+	 *  3 = DOWN
+	 */
 	public int getInput();
 }
