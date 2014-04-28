7d1020e9dd13273093e7731faf1f15c37e13ea6e
Michael Landes <mokonzi131@gmail.com>
2014-04-28 11:36:53 -0400

    implemented a way to visualize agent s score

--- CS 2340 Agent Simulation/src/environment/Astats.java
+++ CS 2340 Agent Simulation/src/environment/Astats.java
@@ -1,8 +1,15 @@
 package environment;
 
+import java.awt.Color;
+
 public class Astats {
 	public int x;
 	public int y;
 	public int score;
 	public int newScore;
+	
+	public Color color(int scale) {
+		float intensity = (float) score / scale * 255;
+		return new Color(255, 0, 0, (int) intensity);
+	}
 }
